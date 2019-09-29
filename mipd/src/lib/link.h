18c36287412c85f88ef5d2114bf1a8880df03b2c
sondre <sondre.hal@gmail.com>
2019-09-29 15:25:18 +0200

    REMOVE unneeded functions from link.c

--- mipd/src/lib/link.h
+++ mipd/src/lib/link.h
@@ -7,15 +7,7 @@
 #include "mip_packet.h"
 
 
-char *macaddr_str(struct sockaddr_ll *sa);
-
 int last_inteface(struct sockaddr_ll *so_name);
-
-int collect_intefaces(struct sockaddr_ll *so_name, int buffer_n);
-
-int receive_raw_mip_packet(int sd, struct ether_frame  *frame_hdr, struct sockaddr_ll *so_name, struct mip_header *header);
-
-int send_raw_mip_packet(int sd, struct sockaddr_ll *so_name, struct ether_frame *frame_hdr, struct mip_header *mip_header);
 
 int sendto_raw_mip_packet(int sd, struct sockaddr_ll *so_name, struct mip_packet *packet);
 
