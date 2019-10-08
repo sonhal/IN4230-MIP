0b31b8594a8ce9158feb3406bddbe276918c4bad
sondre <sondre.hal@gmail.com>
2019-10-08 22:00:41 +0200

    Update routerd main file

--- routerd/src/main/routerd.c
+++ routerd/src/main/routerd.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-#include "../../../commons/src/dbg.h"
 
+#include "../lib/cli.h"
 
 int main(int argc, char *argv[]){
     printf("Routing up!\n");
 
     //get mipd communication socket
-
     //get remote routerd communication socket
+    RouterdConfig *config = parse_args(argc, argv);
+    if(config == NULL) return 0;
 
-    
+
+
+
+    RouterdConfig_destroy(config);
+    return 0;
 }
