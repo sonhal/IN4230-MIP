344571c7adf7aa5b2480961bfe29207ee3083cb1
sondre <sondre.hal@gmail.com>
2019-11-10 00:01:03 +0100

    Implement a skeleton main

--- miptpd/src/main/miptpd.c
+++ miptpd/src/main/miptpd.c
@@ -1,15 +1,22 @@
 #include "../lib/miptp_server.h"
 #include "../lib/miptp_app_controller.h"
 
-#define MIPTPD_HELP_MESSAGE "miptpd [-h] [-d] <mipd_socket> <timeout>"
+#define MIPTPD_HELP_MESSAGE "miptpd [-h] [-d] <application_socket> <timeout>"
 
 
 int main(int argc, char *argv[]){
+    int rc = 0;
     printf("%s\n", MIPTPD_HELP_MESSAGE);
 
-    MIPTPAppController *controller = MIPTPAppController_create(10);
-    printf("max connections: %d", controller->max_connections);
-    MIPTPAppController_destroy(controller);
+    MIPTPServer *server = MIPTPServer_create("mipdsock", "appsock", 1, 1000);
+    rc = MIPTPServer_init(server);
+    check(rc != -1, "Initializion of the MIPTPServer failed, aborting");
+    rc = MIPTPServer_run(server);
+    check(rc != -1, "MIPTPServer exited unexpectedly");
 
     return 0;
+    
+    error:
+        log_err("[MIPTP SERVER] Failed unexpectedly");
+        return 1;
 }
