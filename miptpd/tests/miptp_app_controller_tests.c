5341605838ff31a34a26115818df0b7f6e51ff7d
sondre <sondre.hal@gmail.com>
2019-11-12 11:35:25 +0100

    CHANGE tests

--- miptpd/tests/miptp_app_controller_tests.c
+++ miptpd/tests/miptp_app_controller_tests.c
@@ -7,7 +7,7 @@
 
 
 char *test_MIPTPAppController_handle_new_package() {
-    MIPTPAppController *app_controller = MIPTPAppController_create(2);
+    MIPTPAppController *app_controller = MIPTPAppController_create(1, 10);
     mu_assert(app_controller != NULL, "Failed to create MIPTPAppController");
     
 
@@ -16,16 +16,16 @@ char *test_MIPTPAppController_handle_new_package() {
 
 
 char *test_MIPTPAppController_create() {
-    MIPTPAppController *app_controller = MIPTPAppController_create(5);
+    MIPTPAppController *app_controller = MIPTPAppController_create(1, 5);
     mu_assert(app_controller != NULL, "Failed to create MIPTPAppController");
 
     return NULL;
 }
 
 char *test_MIPTPAppController_destroy() {
-    MIPTPAppController *app_controller = MIPTPAppController_create(5);
+    MIPTPAppController *app_controller = MIPTPAppController_create(1, 5);
     MIPTPAppController_destroy(app_controller);
-    MIPTPAppController *app_controller2 = MIPTPAppController_create(5);
+    MIPTPAppController *app_controller2 = MIPTPAppController_create(1, 5);
     List_destroy(app_controller2->connections);
     app_controller2->connections = NULL; // Check that destroy does not blow up in this case
     MIPTPAppController_destroy(app_controller2);
